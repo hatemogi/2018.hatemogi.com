9fa8f2c9eb44da6b5145ddf6ddf4f00531e290b1
Daehyun Kim <hatemogi@gmail.com>
2018-07-12 20:06:22 +0900

    전체화면 레이아웃

--- app.elm
+++ app.elm
@@ -1,7 +1,7 @@
-import Html exposing (Html, button, nav, div, ul, li, p, a, i,
-                      text, aside, main_, section, footer)
+import Html exposing (Html, button, nav, div, ul, li, p, a, i, span
+                     ,text, aside, main_, section, header, footer)
 import Html.Events exposing (onClick)
-import Html.Attributes exposing (class, classList)
+import Html.Attributes exposing (id, class, classList, attribute, href)
 import Http
 import Markdown
 import Xml.Decode
@@ -47,29 +47,26 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-  div []
-    [menuView model, mainView model, footerView model]
+  section [ class "hero is-info is-fullheight"]
+    [ div [ class "hero-head" ] [ menuView model ]
+    , div [ class "hero-body" ] [ mainView model ]
+    , div [ class "hero-foot" ] [footerView model ]]
 
 menuView : Model -> Html Msg
 menuView model =
-  let
-    menu section label =
-      li [ classList [("is-active", model.section == section)] ]
-         [ a [onClick (Go section)] [ text label ]]
-  in
-    nav [ class "container" ]
-      [ div [ class "tabs" ]
-        [ ul []
-          [ menu S소개 "소개"
-          , menu S프로젝트 "프로젝트"
-          , menu S잡담 "잡담" ]]]
+  header [ class "navbar" ]
+    [ div [ class "container" ]
+      [ div [ class "navbar-brand" ]
+        [ a [ class "navbar-item", href "/"]
+          [ text "hatemogi.com" ]]]]
+
 
 introView : Html Msg
 introView =
   div []
     [text
     """소프트웨어 개발자.
-    어려서 Apple IIe로 독학 프로그래밍에 빠져든 이래 개발을 취미이자
+    어려서 재미삼아 프로그래밍에 빠져든 이래 개발을 취미이자
     직업으로 삼았습니다. 홍익대에서 컴퓨터공학을 전공하고,
     다음커뮤니케이션(현: 카카오)에서 클라우드기술팀 팀장을 거치며 만 10년
     정도 근무한 뒤 퇴사하여, 1인 소프트웨어 개발사의 대표로 지내고 있습니다."""]
@@ -84,21 +81,28 @@ writingsView =
 
 mainView : Model -> Html Msg
 mainView model =
-  main_ []
-    (case model.section of
+  div [ class "container has-text-centered"]
+    [case model.section of
       S소개 ->
-        [introView]
+        introView
       S프로젝트 ->
-        [projectsView]
+        projectsView
       S잡담 ->
-        [writingsView])
+        writingsView]
 
 footerView : Model -> Html Msg
 footerView model =
-  footer [ class "footer" ]
-    [ div [ class "content has-text-centered" ]
-      [ p [] [ text "Copyright 2018 "
-             , i [ class "fab fa-medium fa-lg" ] [] ]]]
+  let
+    menu section label =
+      li [ classList [("is-active", model.section == section)]]
+         [a [onClick (Go section)] [ text label ]]
+  in
+    nav [ class "tabs is-boxed is-fullwidth" ]
+      [ div [ class "container" ]
+        [ ul []
+          [ menu S소개 "소개"
+          , menu S프로젝트 "프로젝트"
+          , menu S잡담 "잡담"]]]
 
 -- SUBSCRIPTIONS
 
