b9cdf8439d8626b087b75d2f59952a16def91ae0
Daehyun Kim <hatemogi@gmail.com>
2018-07-18 19:40:06 +0900

    프로젝트 데이터 준비

---
+++ App.elm
@@ -0,0 +1,128 @@
+import Html exposing (..)
+import Html.Events exposing (onClick)
+import Html.Attributes exposing (..)
+import Http
+import Markdown
+import Xml.Decode
+import Projects
+
+main =
+  Html.program { init = init "테스트", view = view,
+                 update = update, subscriptions = subscriptions }
+
+-- MODEL
+
+type Section = S소개 | S프로젝트 | S잡담
+type alias Model = { section : Section, medium : Maybe String }
+
+init : String -> (Model, Cmd Msg)
+init name =
+  ({ section = S소개, medium = Nothing }, loadMediumFeed)
+
+-- UPDATE
+
+type Msg = Go Section |
+           MediumFeed (Result Http.Error String)
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    Go section ->
+      ({model | section = section}, Cmd.none)
+
+    MediumFeed (Ok content) ->
+      ({model | medium = Just content}, Cmd.none)
+
+    MediumFeed (Err _) ->
+      ({model | medium = Nothing}, Cmd.none)
+
+-- VIEW
+
+view : Model -> Html Msg
+view model =
+  section [ class "section is-info is-fullheight"]
+    [ div [ class "container"] [ mainView model ]
+    , div [ class "hero-foot" ] [footerView model ]]
+
+menuView : Model -> Html Msg
+menuView model =
+  header [ class "navbar" ]
+    [ div [ class "container" ] [] ]
+
+mainView : Model -> Html Msg
+mainView model =
+  div [ class "has-text-centered columns"]
+    [ div [ class "column box is-narrow" ] [ introCardView ]
+    , div [ class "column box has-text-justified" ]
+      [ div []
+        (case model.section of
+          S소개 ->
+            [h1 [ class "title" ] [ text "김대현" ], introView]
+          S프로젝트 ->
+            [h1 [ class "title" ] [ text "프로젝트" ], projectsView]
+          S잡담 ->
+            [h1 [ class "title" ] [ text "잡담" ], writingsView])]]
+
+footerView : Model -> Html Msg
+footerView model =
+  let
+    menu section label =
+      li [ classList [("is-active", model.section == section)]]
+         [a [onClick (Go section)] [ text label ]]
+  in
+    nav [ class "tabs is-boxed is-fullwidth" ]
+      [ div [ class "container" ]
+        [ ul []
+          [ menu S소개 "소개"
+          , menu S프로젝트 "프로젝트"
+          , menu S잡담 "잡담"]]]
+
+introView : Html Msg
+introView =
+  markdown
+    """소프트웨어 개발자.
+어려서 재미삼아 프로그래밍에 빠져든 이래 개발을 취미이자
+직업으로 삼았습니다. 홍익대에서 컴퓨터공학을 전공하고,
+다음커뮤니케이션(현: 카카오)에서 클라우드기술팀 팀장을 거치며 만 10년
+정도 근무한 뒤 퇴사하여, 1인 소프트웨어 개발사의 대표로 지내고 있습니다.
+"""
+
+projectsView : Html Msg
+projectsView =
+  markdown "# 프로젝트"
+
+writingsView : Html Msg
+writingsView =
+  div [] [text "잡담"]
+
+introCardView : Html Msg
+introCardView =
+  div [ class "card", style [("width", "230px")] ]
+    [ div [ class "card-image" ] [img [src "img/profile.jpg"] []]
+    , div [ class "card-content has-text-left" ]
+        [markdown
+         """백엔드 개발자. **이상적 Clojure**와 **현실적 Java**를 오가며 **실용 프로그래밍**.
+            개발에 몰입할 때가 가장 즐거운 걸 보면 아무래도 **백발 개발자**가 될 모양새."""]
+    , footer [ class "card-footer" ]
+      [ p [ class "card-footer-item" ]
+       [span [] [ a [ href "https://medium.com/happyprogrammer-in-jeju"]
+                    [ i [ class "fab fa-medium fa-lg"] []
+                    , text " 둘러보기"]]]]]
+
+markdown : String -> Html Msg
+markdown content =
+  div [] [Markdown.toHtml [] content]
+
+-- SUBSCRIPTIONS
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none
+
+-- HTTP
+
+loadMediumFeed : Cmd Msg
+loadMediumFeed =
+  -- CORS 해결 필요
+  -- Http.send MediumFeed (Http.getString "https://medium.com/feed/happyprogrammer-in-jeju")
+  Cmd.none
---
+++ Projects.elm
@@ -0,0 +1,88 @@
+module Projects exposing (data)
+import Json.Decode exposing (..)
+import Json.Decode.Pipeline exposing (decode, required, optional)
+
+type alias Project =
+  { category    : String
+  , year        : Int
+  , title       : String
+  , url         : Maybe String
+  , role        : String
+  , tags        : List String
+  , description : Maybe String
+  }
+
+projectDecoder : Decoder Project
+projectDecoder =
+  decode Project
+    |> required "category" string
+    |> required "year" int
+    |> required "title" string
+    |> optional "url" (nullable string) Nothing
+    |> required "role" string
+    |> required "tags" (list string)
+    |> optional "description" (nullable string) Nothing
+
+loadProjects : String -> List Project
+loadProjects str =
+  case (decodeString (list projectDecoder) str) of
+    Ok xs -> xs
+    Err _ -> []
+
+data =
+  [ Project "work" 2018 "프로토파이 기업용 서버 백엔드 개발" Nothing "외주개발" ["Clojure", "Ring", "Postgres", "Docker", "AWS"] Nothing
+  , Project "work" 2018 "N사 협업플랫폼용 CardDAV 서버" Nothing "외주개발" ["Java", "Jetty", "Mustache"] Nothing
+  , Project "work" 2017 "N사 협업플랫폼용 LDAP 서버" Nothing "외주개발" ["Java", "Netty", "Spring Boot"] Nothing
+  , Project "work" 2017 "N사 협업플랫폼용 주소록 API 서버" Nothing "외주개발" ["Java", "Spring Boot", "JPA", "MySQL"] Nothing
+  , Project "work" 2016 "N사 협업플랫폼용 CalDAV 서버" Nothing "외주개발" ["Java", "Spring Boot"] Nothing
+  , Project "work" 2015 "N사 협업플랫폼용 메시징 서버" Nothing "외주개발" ["Java", "Akka"] Nothing
+  , Project "work" 2015 "N사 협업플랫폼용 SMTP 서버" Nothing "외주개발" ["Java", "Netty", "Tape"] Nothing
+  , Project "work" 2014 "Daum Tomcat/Node PaaS 시스템 구축" Nothing "팀장" ["Java", "Node", "Ruby"] Nothing
+  , Project "work" 2013 "Daum 사내 Rest API 접근 DNS 시스템 (애조로) 구축" Nothing "팀장" ["팀리딩", "기술영업"] Nothing
+  , Project "work" 2013 "Daum Redis PaaS 시스템 구축" Nothing "팀장" ["팀리딩", "기술영업"] Nothing
+  , Project "work" 2012 "Daum 사내 Git 저장소 시스템 구축 및 운영" Nothing "팀원" ["Ruby", "Sinatra", "OpenSSH", "Git", "SQLite"] Nothing
+  , Project "work" 2012 "Daum 사내 실험 클라우드 플랫폼 구축" Nothing "팀원" ["Ruby", "CloudFoundry"] Nothing
+  , Project "work" 2011 "Daum 한메일 ActiveSync 게이트웨이 서버 개발" Nothing "CTO스태프" ["Ruby", "Sinatra", "Redis"] Nothing
+  , Project "work" 2010 "Daum 클라우드 파일 동기화 프로토콜 디자인 및 SDK 개발" Nothing "CTO스태프" ["Java", "SDK"] Nothing
+  , Project "work" 2010 "Daum 마이피플 네트워크 아키텍쳐 설계 및 초기개발" Nothing "TFT" ["Objective-C", "C"] Nothing
+  , Project "work" 2007 "Daum 캘린더 서비스 개발 및 리딩"
+    (Just "https://medium.com/happyprogrammer-in-jeju/다음-캘린더-서비스의-비하인드-스토리-ec0faac67f05")
+    "TFT장" ["Ruby", "Rails", "MySQL"] Nothing
+  , Project "work" 2006 "Daum 카페 채팅 서비스 부하분산 서버 개발" Nothing "팀원" ["C", "PThread"] Nothing
+  , Project "work" 2005 "Daum 일본, 개인홈 서비스 개발 및 운영" Nothing "팀장" ["Java", "Spring", "MySQL"] Nothing
+  , Project "work" 2004 "Daum 일본, 커뮤니티 서비스 개발 및 운영" Nothing "팀원" ["Java", "Velocity", "MySQL"] Nothing
+  , Project "work" 2003 "Daum 플래닛 개인홈 서비스 개발 및 운영" Nothing "팀원" ["Java", "Struts", "MySQL"] Nothing
+  , Project "work" 2003 "Daum 카페 한줄 게시판, 투표판 개발" Nothing "팀원" ["Java", "Servlet", "FreeMarker"] Nothing
+
+  , Project "hobby" 2017 "스타웍스 - 스타벅스 WiFi 자동연결 macOS 애플리케이션과 개발기" Nothing "개인" ["Swift"] Nothing
+  , Project "hobby" 2016 "http://한글코딩.org/ - 한글로 코딩하자"
+    (Just "http://한글코딩.org/") "개인" ["Clojure"]
+    (Just "변수, 함수, 객체 이름 지을 때, 콩글리시 영작 그만하고, 한글로 코딩하자는 주장을 담은 웹사이트 오픈")
+  , Project "hobby" 2014 "AewolInputMethod - macOS용 한글 입력기 개발"
+    (Just "https://medium.com/happyprogrammer-in-jeju/dvorak과-한글-입력기-개발-8940bc4714a1")
+    "개인" ["Objective-C", "C"] (Just "Dvorak 자판과 한글 두벌식 자판을 함께 쓰기 편한 macOS용 한글 입력기")
+  , Project "hobby" 2010 "귀여운 우주선 게임, xkobo for macOS"
+    (Just "https://github.com/hatemogi/xkobo")
+    "개인" ["GNU C", "X Window System"] (Just "Dvorak 자판과 한글 두벌식 자판을 함께 쓰기 편한 macOS용 한글 입력기")
+  , Project "hobby" 1998 "귀여운 우주선 게임, xkobo for Win32" Nothing
+    "개인" ["Delphi", "Win32"] (Just "X윈도우용 오픈소스 게임을 MS윈도용으로 포팅")
+  , Project "hobby" 1995 "3인용 테트리스 게임"
+    (Just "http://blog.naver.com/PostView.nhn?blogId=taksangs&logNo=60050324463")
+    "개인" ["Borland Pascal", "Assembly", "MS-DOS"] (Just "직접 개발한 VESA 그래픽 라이브러리를 이용한 게임 개발")
+
+  , Project "talk" 2017 "한글 코딩 도구 비지니스 피칭" Nothing "개인" ["Clojure"] Nothing
+  , Project "talk" 2016 "PyCon 라이트닝토크 - 한글코딩 5분 발표"
+    (Just "https://youtu.be/46UkzB-3z3Y")
+    "개인" ["Python", "Clojure"]
+    (Just "[발표후기](https://medium.com/happyprogrammer-in-jeju/파이콘-2016-라이트닝-토크-발표-후기-763135a2a623")
+  , Project "talk" 2013 "Daum DevOn 컨퍼런스 - 사내 Git 저장소 개발사례"
+    (Just "https://www.slideshare.net/hatemogi/devon2013-git")
+    "개인" [] Nothing
+
+  , Project "번역" 2000 "유의미 버전 SemVer 한글 번역"
+    (Just "https://semver.org/lang/kr") "개인" [] Nothing
+  , Project "번역" 2000 "클로저 선문답 - Clojure Koans 한글 번역"
+    (Just "https://clojurekoans.hatemogi.com") "개인" [] Nothing
+  , Project "번역" 2000 "Ring - 클로저 웹서버 인터페이스 스펙 및 라이브러리 문서 번역"
+    (Just "https://github.com/hatemogi/ring/wiki") "개인" [] Nothing
+  ]
